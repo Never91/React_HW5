341f4f41f5d0e6843f75f16c85e4bb559fe79394
never91 <maxym.88888888@gmail.com>
2021-02-13 11:40:51 +0200

    added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const nav = ({link}) => (
+<nav>
+    <ul>
+        <li>
+         {"Google"}
+        </li>
+        <li>
+         {"Yahoo"}
+        </li>
+        <li>
+         {"Link"}
+        </li>
+    </ul>
+</nav>
+
+);

fd13d6d52067d731d06263c0ec77b8d73598b32a
never91 <maxym.88888888@gmail.com>
2021-02-13 12:06:12 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// const nav = ({link}) => (
-// <nav>
-//     <ul>
-//         <li> {"Google"}</li>
-//         <li> {"Yahoo"}</li>
-//         <li>{"Link"}</li>
-//     </ul>
-// </nav>
-// );
-
-// ReactDOM.render (document.getElementById("root"));
 
 import React from 'react';
 import ReactDOM from 'react-dom';
